276d3d1b33c6df9d26924a8eb8a6a4a02fe0bd02
Bernard Helyer <b.helyer@gmail.com>
2024-07-07 13:50:45 +1200

    vm: Read binary file from disk

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,72 @@
-#include "Pinot/Memory.h"
-#include "Pinot/VM.h"
+#include <format>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <stdexcept>
 
+#include "Pinot/VM.h"
+#include "Pinot/Memory.h"
+
+[[nodiscard]] std::vector<uint8_t> load(const std::string& filename);
+[[nodiscard]] int run(const std::vector<uint8_t>& prog);
+
+// Read a binary file into memory and run the VM on it.
 int main(int argc, char** argv)
 {
-    // load r0, 42      2 0 42
-    // halt             1
-    std::vector<uint8_t> prog{2, 0, 42, 1};
-    Pinot::Memory mem(prog.size() + 1);
+    if (argc != 2)
+    {
+        std::cerr << "usage: " << argv[0] << " file_to_run.bin\n";
+        return 1;
+    }
+
+    try
+    {
+        const std::vector<uint8_t> prog = load(argv[1]);
+        return run(prog);
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "error: " << e.what() << '\n';
+        return 1;
+    }
+    
+    // Never reached.
+}
+
+// Load a file into a vector of bytes, or throw a runtime_error.
+std::vector<uint8_t> load(const std::string& filename)
+{
+    // Open the file.
+    std::ifstream ifs(filename, std::ios::in | std::ios::binary);
+    if (!ifs)
+    {
+        auto msg = std::format("Failed to open '{}' for reading.", filename);
+        throw std::runtime_error(msg);
+    }
+
+    // Calculate the size of the file.
+    ifs.seekg(0, std::ios::end);
+    const auto size = ifs.tellg();
+    ifs.seekg(0);  // Rewind.
+
+    // Read the data into a vector.
+    std::vector<uint8_t> data(size);
+    ifs.read(reinterpret_cast<char*>(data.data()), data.size());
+    if (!ifs)
+    {
+        auto msg = std::format("Failed to read '{}'.", filename);
+        throw std::runtime_error(msg);
+    }
+
+    return data;
+}
+
+// Construct a VM with the given program loaded, run it, and return R0.
+int run(const std::vector<uint8_t>& prog)
+{
+    Pinot::Memory mem(prog.size());
     mem.copy(0, prog);
     Pinot::VM vm(mem);
     vm.run();
-    return vm.read64(Pinot::Register::R0);
+    return static_cast<int>(vm.read64(Pinot::Register::R0));
 }
